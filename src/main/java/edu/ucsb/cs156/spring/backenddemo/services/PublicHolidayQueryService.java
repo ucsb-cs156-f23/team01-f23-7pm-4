5ebf286ab73c185ec9320565b794ce1cf9c9c2d5
Audrey Zhu <audreyzhu059@gmail.com>
2023-10-17 14:51:19 -0700

    azhu - added endpoint and uriVariables for public holiday

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/PublicHolidayQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/PublicHolidayQueryService.java
@@ -20,9 +20,10 @@ public class PublicHolidayQueryService {
         restTemplate = restTemplateBuilder.build();
     }
 
-    public static final String ENDPOINT = "";
+    public static final String ENDPOINT = "https://date.nager.at/api/v2/publicholidays/{year}/{countryCode}";
 
     public String getJSON(String year, String countryCode) throws HttpClientErrorException {
+        Map<String, String> uriVariables = Map.of("year", year, "countryCode", countryCode);
         return "";
     }
 
