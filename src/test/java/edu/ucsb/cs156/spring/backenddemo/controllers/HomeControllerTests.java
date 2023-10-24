bac120c6486c6c4ef90594660db7daab70c0ab5b
Audrey Zhu <azhu@cs.ucsb.edu>
2023-10-24 14:13:35 -0700

    finish tests for Home Controller

--- src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/HomeControllerTests.java
+++ src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/HomeControllerTests.java
@@ -49,12 +49,14 @@ public class HomeControllerTests {
         String baseUrl = "http://localhost:8080/";
 
         List<String> team = new ArrayList<String>();
-        team.add("Jing P.");
-        team.add("Christian S.");
-        team.add("Guy W.");
-        team.add("Phill C.");
+        team.add("Alice Zhou");
+        team.add("Audrey Zhou");
+        team.add("Audrey Zhu");
+        team.add("Benjamin Conte");
+        team.add("Eric Marzouk");
+        team.add("Sasha Nisavic");
         resultMap.put("team",team);
-        resultMap.put("repo","https://github.com/ucsb-cs156-f23/STARTER-team01");
+        resultMap.put("repo","https://github.com/ucsb-cs156-f23/team01-f23-7pm-4");
         resultMap.put("api-documentation", baseUrl + "swagger-ui/index.html");
         String expected = mapper.writeValueAsString(resultMap);
         String actual = HomeController.getHomePageObjectJSON("http://localhost:8080/");
